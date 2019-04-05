0a732ec3a53842a9006e0c11766213186c857027
PinheiroFeliz <joaopinheirosantos@hotmail.com>
2019-04-05 10:09:19 -0300

    exibiçao do movel com filtro e movel sem icollection

---
+++ GerenciamentoMovelVendas/GerenciamentoMovelVendas/Controllers/FuncionariosController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GerenciamentoMovelVendas.Models;
+
+namespace GerenciamentoMovelVendas.Controllers
+{
+    public class FuncionariosController : Controller
+    {
+        private GerenciamentoMovelVendasContext db = new GerenciamentoMovelVendasContext();
+
+        // GET: Funcionarios
+        public ActionResult Index()
+        {
+            var funcionarios = db.Funcionarios.Include(f => f.Movel);
+            return View(funcionarios.ToList());
+        }
+
+        // GET: Funcionarios/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Funcionario funcionario = db.Funcionarios.Find(id);
+            if (funcionario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionario);
+        }
+
+        // GET: Funcionarios/Create
+        public ActionResult Create()
+        {
+            ViewBag.PK_Movel = new SelectList(db.Movels.Where(a => a.Status == Status.Solicitado), "MovelId", "Nome");//Aqui filtra 
+            return View();
+        }
+
+        // POST: Funcionarios/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "FuncionarioId,Nome,PK_Movel")] Funcionario funcionario)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Movel movel = db.Movels.Find(funcionario.PK_Movel);
+                funcionario.Movel = movel;
+                funcionario.Mudar_Status_Movel();
+                db.Funcionarios.Add(funcionario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.PK_Movel = new SelectList(db.Movels, "MovelId", "Nome", funcionario.PK_Movel);
+            return View(funcionario);
+        }
+
+        // GET: Funcionarios/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Funcionario funcionario = db.Funcionarios.Find(id);
+            if (funcionario == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.PK_Movel = new SelectList(db.Movels, "MovelId", "Nome", funcionario.PK_Movel);
+            return View(funcionario);
+        }
+
+        // POST: Funcionarios/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "FuncionarioId,Nome,Status_Disponibilidade_Funcionario,PK_Movel")] Funcionario funcionario)
+        {
+            if (ModelState.IsValid)
+            {
+                Movel movel = db.Movels.Find(funcionario.PK_Movel);
+                funcionario.Movel = movel;
+                funcionario.Mudar_Status_Funcionario_Movel();
+                db.Entry(funcionario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.PK_Movel = new SelectList(db.Movels, "MovelId", "Nome", funcionario.PK_Movel);
+            return View(funcionario);
+        }
+
+        // GET: Funcionarios/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Funcionario funcionario = db.Funcionarios.Find(id);
+            if (funcionario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(funcionario);
+        }
+
+        // POST: Funcionarios/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Funcionario funcionario = db.Funcionarios.Find(id);
+            db.Funcionarios.Remove(funcionario);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GerenciamentoMovelVendas/GerenciamentoMovelVendas/Models/Movel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GerenciamentoMovelVendas.Models
+{
+    public enum Status { Solicitado, Em_Construcao, Entregue }
+    public class Movel
+    {
+        public int MovelId { get; set; }
+        public string Nome { get; set; }
+        public string Cor { get; set; }
+        public Medida Medida { get; set; }
+        public string Material { get; set; }
+        public string LinkImagemMovel { get; set; }
+        public Status Status { get; set; }
+        public virtual Funcionario Funcionario{ get; set; }
+
+
+        public Movel()
+        {
+            Status = Status.Solicitado;
+        }
+    }
+}
